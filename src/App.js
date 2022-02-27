2c2b550a17c5e65fc627667d26b904c24d22532d
Hanchon <guillermo.paoletti@gmail.com>
2022-02-27 20:41:42 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let date_ob = new Date();
+  let dia_final = new Date();
+  dia_final.setUTCFullYear(2023, 1, 15);
+
+  var dias_restantes = dia_final.getTime() - date_ob.getTime();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hoy es {date_ob.toLocaleDateString()}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Faltan {dias_restantes / (1000 * 3600 * 24)} dias para que vicky sea
+          libre!
         </a>
       </header>
     </div>
